70328fa8b0ed34e191396ac265302a0a1661e7d6
xushijie <15101587790@163.com>
2022-08-10 01:01:55 +0800

    feat(j-stack/index.js): add union-find algorithms
    add union-find algorithms and modify j-stack index.js

--- packages/j-stack/src/index.js
+++ packages/j-stack/src/index.js
@@ -133,7 +133,132 @@ class CustomIterator {
     }
 }
 
+
+class QuickFind {
+    constructor( counts ) {
+        this.counts = counts;
+        this.id = [];
+        this.init();
+    }
+
+    init() {
+        for ( let i = 0; i < this.counts; i++ ) {
+            this.id[ i ] = i;
+        }
+    }
+
+    count() {
+        return this.counts;
+    }
+
+    connected( p, q ) {
+        return this.find( p ) === this.find( q );
+    }
+
+    find( p ) {
+        return this.id[ p ];
+    }
+
+    union( p, q ) {
+        const pId = this.find( p );
+        const qId = this.find( q );
+        if ( pId === qId ) return;
+        for ( let i = 0; i < this.id.length; i++ ) {
+            if ( this.id[ i ] === pId ) {
+                this.id[ i ] = qId;
+            }
+        }
+        this.counts--;
+    }
+}
+
+class QuickUnion {
+    constructor( counts ) {
+        this.counts = counts;
+        this.id = [];
+        this.init();
+    }
+
+    init() {
+        for ( let i = 0; i < this.counts; i++ ) {
+            this.id[ i ] = i;
+        }
+    }
+
+    count() {
+        return this.counts;
+    }
+
+    connected( p, q ) {
+        return this.find( p ) === this.find( q );
+    }
+
+    find( p ) {
+        while ( p !== this.id[ p ] ) {
+            p = this.id[ p ];
+        }
+        return p;
+    }
+
+    union( p, q ) {
+        const pRoot = this.find( p );
+        const qRoot = this.find( q );
+        if ( pRoot === qRoot ) return;
+        this.id[ pRoot ] = qRoot;
+        this.counts--;
+    }
+}
+
+class WeightedQuickUnionUf {
+    constructor( counts ) {
+        this.counts = counts;
+        this.sz = [];
+        this.id = [];
+        this.init();
+    }
+
+    init() {
+        for ( let i = 0; i < this.counts; i++ ) {
+            this.id[ i ] = i;
+            this.sz[ i ] = 1;
+        }
+    }
+
+    count() {
+        return this.counts;
+    }
+
+    connected( p, q ) {
+        return this.find( p ) === this.find( q );
+    }
+
+    find( p ) {
+        while ( p !== this.id[ p ] ) {
+            p = this.id[ p ];
+        }
+        return p;
+    }
+
+    union( p, q ) {
+        const pRoot = this.find( p );
+        const qRoot = this.find( q );
+        if ( pRoot === qRoot ) return;
+        if ( this.sz[ pRoot ] < this.sz[ qRoot ] ) {
+            this.id[ pRoot ] = qRoot;
+            this.sz[ qRoot ] += this.sz[ pRoot ];
+        } else {
+            this.id[ qRoot ] = pRoot;
+            this.sz[ pRoot ] += this.sz[ qRoot ];
+        }
+        this.counts--;
+    }
+}
+
 console.log( new Bag() );
 console.log( new Queue() );
 console.log( new Stack() );
 console.log( new CustomIterator() );
+console.log( new QuickFind() );
+console.log( new QuickUnion() );
+console.log( new WeightedQuickUnionUf() );
+
