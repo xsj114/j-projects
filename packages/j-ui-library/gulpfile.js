63f41053196b4fc1e82d11cbb5bd215b12c316a0
xushijie <15101587790@163.com>
2022-11-17 20:56:05 +0800

    fix(j-ui-library): eslint fix

--- packages/j-ui-library/gulpfile.js
+++ packages/j-ui-library/gulpfile.js
@@ -1,25 +1,25 @@
-const { 
+const {
     series,
     parallel,
     src,
-    dest
+    dest,
 } = require( 'gulp' );
-let stylus = require('gulp-stylus');
-let clean = require('gulp-clean');
+const stylus = require( 'gulp-stylus' );
+const clean = require( 'gulp-clean' );
 
 const compileStylus = () => {
     return src( [ 'src/packages/theme/*.styl' ] )
-    .pipe( stylus() )
-    .pipe( dest( 'lib/theme' ))
-}
+        .pipe( stylus() )
+        .pipe( dest( 'lib/theme' ) );
+};
 
 const compileFonts = () => {
-    return src( [ 'src/packages/theme/fonts/*' ] ).pipe( dest( 'lib/theme/fonts' ) )
-}
+    return src( [ 'src/packages/theme/fonts/*' ] ).pipe( dest( 'lib/theme/fonts' ) );
+};
 
 
 const cleanDir = async () => {
-    return src( 'lib/theme/*' ).pipe( clean() )
-}
+    return src( 'lib/theme/*' ).pipe( clean() );
+};
 
-exports.default = series( cleanDir, parallel( compileStylus, compileFonts ) )
+exports.default = series( cleanDir, parallel( compileStylus, compileFonts ) );
