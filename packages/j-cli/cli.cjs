261e3768a2f81006399b4b98fd5b963f12df79bc
shijie.xu <shijie.xu@insgeek.com>
2023-03-28 21:56:24 +0800

    feat(j-cli): develop j-cli

--- packages/j-cli/cli.cjs
+++ packages/j-cli/cli.cjs
@@ -6,12 +6,17 @@ const path = require( 'path' );
 const process = require( 'process' );
 const { exec } = require( 'child_process' );
 const prompts = require('./prompt/index.cjs')
+const generator = require('./lib/generator/index.cjs')
 
-        console.log()
 
 inquirer.prompt( prompts ).then( res => {
     for (let val of res.features) {
-        const module = require( path.join(__dirname, `./lib/template/${val}`) )
-        generator.extend(module) 
+        try{
+            const module = require( path.join(__dirname, `./lib/template/${val}`) )
+            console.log(module)
+            module && ( generator.extend( module ) )
+        }catch(e){
+
+        }
     }
 } )
